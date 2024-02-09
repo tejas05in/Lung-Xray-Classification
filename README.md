0258675e529d2e265e250e20eb07a6cf285040fc
Dr. TEJAS. J <tejas05in@yahoo.co.in>
2024-02-09 15:23:33 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# Lung X-ray Classification
+
+Data link: https://drive.google.com/file/d/1pfIAlurfeqFTbirUZ5v_vapIoGPgRiXY/view?usp=sharing
+
+
+## Workflows
+
+- constants
+- config_enity
+- artifact_enity
+- components
+- pipeline
+- main
+
+# How to setup:
+
+```bash
+conda create -n lungs python=3.8 -y
+```
+
+```bash
+conda activate lungs
+```
+
+```bash
+pip install -r requirements.txt
+```
+
+
+```bash
+setup AWS CLI
+link: https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
+
+```
+
+```bash
+aws configure
+```
+
+```bash
+AWS_ACCESS_KEY_ID=***
+
+AWS_SECRET_ACCESS_KEY= ***
+
+AWS_REGION = us-east-1
+```
+
+
+## BentoML demo repo:
+
+https://github.com/entbappy/bentoml-demo
+
+
---
+++ xray/pipeline/train_pipeline.py
@@ -0,0 +1,146 @@
+import sys
+
+from xray.components.data_ingestion import DataIngestion
+# from xray.components.data_transformation import DataTransformation
+# from xray.components.model_training import ModelTrainer
+
+from xray.entity.artifacts_entity import (
+    DataIngestionArtifact,
+    # DataTransformationArtifact,
+    # ModelTrainerArtifact,
+
+   
+)
+
+
+from xray.entity.config_entity import (
+    DataIngestionConfig,
+    # DataTransformationConfig,
+    # ModelTrainerConfig,
+
+  
+)
+
+
+from xray.exception import XRayException
+from xray.logger import logging
+
+
+
+
+class TrainPipeline:
+    def __init__(self):
+        self.data_ingestion_config = DataIngestionConfig()
+        # self.data_transformation_config = DataTransformationConfig()
+        # self.model_trainer_config = ModelTrainerConfig()
+
+
+
+      
+    def start_data_ingestion(self) -> DataIngestionArtifact:
+        logging.info("Entered the start_data_ingestion method of TrainPipeline class")
+        try:
+
+            logging.info("Getting the data from s3 bucket")
+
+            data_ingestion = DataIngestion(
+                data_ingestion_config=self.data_ingestion_config,
+            )
+
+            data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+
+            logging.info("Got the train_set and test_set from s3")
+
+            logging.info(
+                "Exited the start_data_ingestion method of TrainPipeline class"
+            )
+
+            return data_ingestion_artifact
+
+        except Exception as e:
+            raise XRayException(e, sys)
+        
+
+    
+
+    # def start_data_transformation(
+    #     self, data_ingestion_artifact: DataIngestionArtifact
+    # ) -> DataTransformationArtifact:
+    #     logging.info(
+    #         "Entered the start_data_transformation method of TrainPipeline class"
+    #     )
+
+    #     try:
+    #         data_transformation = DataTransformation(
+    #             data_ingestion_artifact=data_ingestion_artifact,
+    #             data_transformation_config=self.data_transformation_config,
+    #         )
+
+    #         data_transformation_artifact = (
+    #             data_transformation.initiate_data_transformation()
+    #         )
+
+    #         logging.info(
+    #             "Exited the start_data_transformation method of TrainPipeline class"
+    #         )
+
+    #         return data_transformation_artifact
+
+    #     except Exception as e:
+    #         raise XRayException(e, sys)
+        
+
+    
+
+
+    # def start_model_trainer(
+    #     self, data_transformation_artifact: DataTransformationArtifact
+    # ) -> ModelTrainerArtifact:
+    #     logging.info("Entered the start_model_trainer method of TrainPipeline class")
+
+    #     try:
+    #         model_trainer = ModelTrainer(
+    #             data_transformation_artifact=data_transformation_artifact,
+    #             model_trainer_config=self.model_trainer_config,
+    #         )
+
+    #         model_trainer_artifact = model_trainer.initiate_model_trainer()
+
+    #         logging.info("Exited the start_model_trainer method of TrainPipeline class")
+
+    #         return model_trainer_artifact
+
+    #     except Exception as e:
+    #         raise XRayException(e, sys)
+
+
+
+
+
+    
+  
+
+    def run_pipeline(self) -> None:
+        logging.info("Entered the run_pipeline method of TrainPipeline class")
+
+        try:
+            data_ingestion_artifact: DataIngestionArtifact = self.start_data_ingestion()
+            # data_transformation_artifact: DataTransformationArtifact = (
+            #     self.start_data_transformation(
+            #         data_ingestion_artifact=data_ingestion_artifact
+            #     )
+            # )
+
+
+            # model_trainer_artifact: ModelTrainerArtifact = self.start_model_trainer(
+            #     data_transformation_artifact=data_transformation_artifact
+            # )
+
+
+        
+           
+
+            logging.info("Exited the run_pipeline method of TrainPipeline class")
+
+        except Exception as e:
+            raise XRayException(e, sys)
